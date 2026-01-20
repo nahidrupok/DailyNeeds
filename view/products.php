27409ae33cc69359606225b6097c9c7fe0b8a12b
RupokAIUB <nahidrupok581@gmail.com>
2026-01-20 17:42:36 +0600

    Modifyed

--- view/products.php
+++ view/products.php
@@ -40,11 +40,11 @@ $demoImgBase = "https://images.unsplash.com/photo-1542838132-92c53300491e?auto=f
 <body>
 
     <nav>
-        <a href="index.php" class="logo">DailyNeeds</a>
+        <a href="../index.php" class="logo">DailyNeeds</a>
         <div class="nav-links">
             <a href="products.php">Products</a>
             <a href="login.php">Login</a>
-            <a href="register.php">Register</a>
+            <a href="registration.php">Register</a>
         </div>
     </nav>
 
